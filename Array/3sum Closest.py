21aeb81fe58e33f77e3c3b0d2279b591a05ef242
Nikhil Manu <70922592+NikhilManu@users.noreply.github.com>
2021-07-30 23:53:05 +0530

    Update 3sum Closest.py

--- Array/3sum Closest.py
+++ Array/3sum Closest.py
@@ -4,10 +4,10 @@
 def threeSumCLosest(array, target):
     array.sort()
     res = float('inf')
-    for i in range(len(nums) - 2):
+    for i in range(len(array) - 2):
         l, r = i + 1, len(nums) - 1
         while l < r:
-            currentSum = nums[i] + nums[l] + nums[r]
+            currentSum = array[i] + array[l] + array[r]
             newDifference = abs(target - currentSum)
             currentDifference = abs(target - res)
             res = currentSum if newDifference < currentDifference else res
