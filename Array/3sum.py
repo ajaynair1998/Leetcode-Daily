bfafa6342fe6ca8939faca2a6e83df51053d2844
Nikhil Manu <70922592+NikhilManu@users.noreply.github.com>
2021-07-30 22:59:51 +0530

    Create 3sum.py

---
+++ Array/3sum.py
@@ -0,0 +1,26 @@
+# Solution 
+# Time O(n^2) | Space O(n)
+
+def 3sum(array):
+    nums.sort()
+    result = []
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            currentSum = nums[i] + nums[l] + nums[r]
+            if currentSum < 0:
+                l += 1
+            elif currentSum > 0:
+                r -= 1
+            else:
+                result.append( [nums[i], nums[l], nums[r] )
+                while l < r and nums[l] == nums[l + 1]:
+                    l += 1
+                while l < r and nums[r] == nums[r - 1]:
+                    r -= 1
+                r += 1
+                l -= 1
+                
+    return result
