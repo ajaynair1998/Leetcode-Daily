6de09b852410889d6dfe254b109cba15f419ef27
AJAY <ajaynairarun@gmail.com>
2021-07-31 23:50:27 +0530

    findduplicatecycledetection

---
+++ arrayProblems/findTheDuplicateNumber.js
@@ -0,0 +1,30 @@
+function findTheDuplicateNumber(nums)
+{
+    // since we cannot modify the array
+    // we have to use cycle detection like in linked list \
+    // by using two pointers
+
+    let pointerOne=nums[0]
+    let pointerTwo=nums[0]
+
+    while (true)
+    {
+        pointerOne=nums[pointerOne]
+        pointerTwo=nums[nums[pointerTwo]]
+
+        if(pointerOne === pointerTwo)
+            {
+                break
+            }
+    }
+
+    pointerOne=nums[0]
+    while(pointerOne !== pointerTwo)
+    {
+        pointerOne=nums[pointerOne]
+        pointerTwo=nums[pointerTwo]
+    }
+
+    return pointerTwo
+};
+}
