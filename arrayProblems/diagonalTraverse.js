31e65bcd2e3d904fa8a723f30392fd9998fa1432
AJAY <ajaynairarun@gmail.com>
2021-08-08 12:03:03 +0530

    diagonal traverse

---
+++ arrayProblems/diagonalTraverse.js
@@ -0,0 +1,40 @@
+function findDiagonalOrder(mat)
+{
+    // to store the diagonals
+    let map={}
+    // final array to store diagonal
+    let finalArray=[]
+
+    for (let i=0;i<mat.length;i++)
+    {
+        for (let j=0;j<mat[0].length;j++)
+        {
+            if(map[i+j])
+            {
+                map[i+j].push(mat[i][j])
+            }
+            else
+            {
+                map[i+j] = [mat[i][j]]
+            }
+        }
+    }
+    console.log(map)
+
+    for(let diagonal in map)
+    {   
+        if(diagonal % 2 === 0)
+        {
+            map[diagonal].reverse().forEach(item => finalArray.push(item) )
+        }
+        else
+        {
+        map[diagonal].forEach(item => finalArray.push(item) )
+        }
+    }
+
+    return finalArray
+    
+}
+
+console.log(findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
