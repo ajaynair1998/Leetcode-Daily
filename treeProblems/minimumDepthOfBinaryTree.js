02fd08954f3f406cb7e02b8bc61224d36dffc7f0
AJAY <ajaynairarun@gmail.com>
2021-08-17 22:58:59 +0530

    minimum depth of binary tree

---
+++ treeProblems/minimumDepthOfBinaryTree.js
@@ -0,0 +1,25 @@
+function minimumDepth(root)
+{
+    
+    return traverse(root,0)
+}
+
+function traverse(root,currentLength)
+{
+    if(!root)return currentLength
+
+    if(root.left === null )
+    {
+        return traverse(root.right,currentLength+1)
+    }
+
+    if(root.right === null )
+    {
+        return traverse(root.left,currentLength+1)
+    }
+
+    let leftBranch=traverse(root.left,currentLength+1)
+    let rightBranch=traverse(root.right,currentLength+1)
+
+    return Math.min(leftBranch,rightBranch)
+}
