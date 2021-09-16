fe956d56aae70b419a7c53eec1bc948d5a2d5d6a
ajay <ajaynairarun@gmail.com>
2021-09-16 10:19:27 +0530

    merged

---
+++ stringProblems/longestPalindrome.js
@@ -0,0 +1,12 @@
+function longestPalindrome(s)
+{
+    let reversed=''
+    let leftpointer=0
+    let currentMaximum={size:0,string:""}
+
+
+    for(let rightPointer=0;rightPointer<s.length;rightPointer++)
+    {
+        
+    }
+}
