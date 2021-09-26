01ad59755fd228215cb1067ea1b4704382fccb49
AJAY <ajaynairarun@gmail.com>
2021-09-26 18:35:16 +0530

    changed up palindromic substrings

--- stringProblems/palindromicSubstrings.js
+++ stringProblems/palindromicSubstrings.js
@@ -1,4 +1,4 @@
-function countSubstrings(s) {
+var countSubstrings = function (s) {
   let numberOfSubstrings = 0;
 
   for (let centre = 0; centre < s.length; centre++) {
@@ -8,16 +8,13 @@ function countSubstrings(s) {
     numberOfSubstrings = numberOfSubstrings + evenSubstrings + oddSubstrings;
   }
   return numberOfSubstrings + s.length;
-}
-
+};
 function expandFromCentre(s, left, right) {
-  let numberOfSubstrings = 0;
-
-  for (let centre = 0; centre < s.length; centre++) {
-    let evenSubstrings = expandFromCentre(s, centre, centre + 1);
-    let oddSubstrings = expandFromCentre(s, centre - 1, centre + 1);
-    console.log(evenSubstrings, oddSubstrings);
-    numberOfSubstrings = numberOfSubstrings + evenSubstrings + oddSubstrings;
+  let palindromesInCurrentCentre = 0;
+  while (left >= 0 && right < s.length && s[left] == s[right]) {
+    left--;
+    right++;
+    palindromesInCurrentCentre++;
   }
-  return numberOfSubstrings + s.length;
+  return palindromesInCurrentCentre;
 }
