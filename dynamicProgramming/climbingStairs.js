87eb80dbee2a959d5b563e64a1cbe0e901794424
AJAY <ajaynairarun@gmail.com>
2021-09-26 18:31:19 +0530

    change

--- dynamicProgramming/climbingStairs.js
+++ dynamicProgramming/climbingStairs.js
@@ -1,36 +1,33 @@
 function climbStairs(n) {
-  if(n < 3)return n
+  if (n < 3) return n;
 
-  let first=1
-  let second=2
+  let first = 1;
+  let second = 2;
 
-  for(let i=3;i<n;i++){
-    let current=first+second
-    first=second
-    second=current
+  for (let i = 3; i < n; i++) {
+    let current = first + second;
+    first = second;
+    second = current;
   }
-  return second
+  return second;
 }
 
-
-function climbStairsrecursive(n,memo={})
-{
-  if(n==1){
-    return 1
+function climbStairsrecursive(n, memo = {}) {
+  if (n == 1) {
+    return 1;
   }
-  if(n==2){
-    return 2
+  if (n == 2) {
+    return 2;
   }
 
-  if(memo[n]!==undefined){
-    return memo[n]
+  if (memo[n] !== undefined) {
+    return memo[n];
   }
 
-  let result=climbStairsrecursive(n-1,memo) + climbStairsrecursive(n-2,demo)
-  memo[n]=result
-  return result
-
-
+  let result =
+    climbStairsrecursive(n - 1, memo) + climbStairsrecursive(n - 2, demo);
+  memo[n] = result;
+  return result;
 }
 
-console.log(climbStairsrecursive(2))
+console.log(climbStairsrecursive(2));
