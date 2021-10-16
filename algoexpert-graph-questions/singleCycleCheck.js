2afc50abe8cdd0d2ca373211415cdf21f777671f
AJAY <ajaynairarun@gmail.com>
2021-10-16 23:13:22 +0530

    has single cycle

---
+++ algoexpert-graph-questions/singleCycleCheck.js
@@ -0,0 +1,19 @@
+function hasSingleCycle(array) {
+  // Write your code here.
+  let counter = array.length;
+  let currentIndex = 0;
+  while (counter) {
+    if (counter < array.length && currentIndex == 0) return false;
+    counter--;
+    currentIndex = getNextIndex(array, currentIndex);
+  }
+  return currentIndex == 0;
+}
+
+function getNextIndex(array, currentIndex) {
+  let jumpTimes = array[currentIndex];
+  let lengthOfArray = array.length;
+  let nextPosition = (currentIndex + jumpTimes) % lengthOfArray;
+
+  return nextPosition >= 0 ? nextPosition : nextPosition + lengthOfArray;
+}
