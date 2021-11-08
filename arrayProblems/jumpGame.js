f8a3f0a65ae480f3a8494cbbf770718330725bbb
ajay <ajaynairarun@gmail.com>
2021-11-08 17:51:57 +0530

    jumpgame2 created

--- arrayProblems/jumpGame.js
+++ arrayProblems/jumpGame.js
@@ -2,6 +2,7 @@ function canJump(nums) {
   let max = 0;
 
   for (let i = 0; i < nums.length; i++) {
+    if (max > nums.length) return true;
     if (max < i) return false;
     max = Math.max(i + nums[i], max);
   }
